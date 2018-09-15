17ce5a16966f37f59aa187c81b8e16d2c0b09c1d
elishakay <kramer1346@gmail.com>
2018-09-15 23:30:08 +0300

    Crowd-It Email and Run Campaign button text

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -32,7 +32,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
         onClick={() => submitSurvey(formValues, history)}
         className="green btn-flat right white-text"
       >
-        Send Survey
+        Run Campaign
         <i className="material-icons right">email</i>
       </button>
     </div>
