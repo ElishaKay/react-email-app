bf1122280f96011e554321f530dfb2b6a82a550a
elishakay <kramer1346@gmail.com>
2018-10-09 10:03:57 +0300

    one perfect silkn email template

---
+++ services/emailTemplates/silknTemplate.js
@@ -0,0 +1,194 @@
+const keys = require('../../config/keys');
+
+module.exports = survey => {
+  return `
+	 <div style=3D"margin:0;outline:none;padding:0"> <div style=3D"margin:0;out=
+	line:none;padding:0"> <div style=3D"margin:0;outline:none;padding:0;color:#=
+	084b8a;font-size:18px"> <div style=3D"margin:0;outline:none;padding:0;color=
+	:#084b8a"> <span style=3D"color:#084b8a;font-size:16px;font-weight:inherit;=
+	line-height:inherit;text-decoration:inherit"> </span><p style=3D"margin:0;o=
+	utline:none;padding:0;color:#084b8a;font-size:inherit;font-weight:inherit;l=
+	ine-height:inherit;text-decoration:inherit;text-align:left"></p>
+	</div>
+	 </div>
+	 <span style=3D"color:#084b8a;font-size:inherit;font-weight:inherit;line-he=
+	ight:inherit;text-decoration:inherit"><span style=3D"color:#084b8a;font-siz=
+	e:16px;font-weight:inherit;line-height:inherit;text-decoration:inherit;font=
+	-family:arial;font-style:normal">Hi</span><span style=3D"color:#084b8a;font=
+	-size:16px;font-weight:inherit;line-height:inherit;text-decoration:inherit;=
+	font-family:arial;font-style:normal">,</span><br style=3D"color:#084b8a"><b=
+	r><span style=3D"color:#084b8a;font-size:16px;font-weight:inherit;line-heig=
+	ht:inherit;text-decoration:inherit;font-family:arial;font-style:normal">We =
+	are offering you first, our past Silk&#39;n clients, the chance to purchase=
+	 our new:</span><br style=3D"color:#084b8a"><br style=3D"color:#084b8a"><di=
+	v style=3D"margin:0;outline:none;padding:0;text-align:center;font-size:18px=
+	;color:#084b8a"><span style=3D"color:#084b8a;font-size:inherit;font-weight:=
+	inherit;line-height:inherit;text-decoration:inherit;font-family:arial;text-=
+	align:inherit"><a href=3D"http://silkncure.acemlnc.com/lt.php?s=3Dcbfc8d9a9=
+	a09b07c2c5410626a8a504d&amp;i=3D137A278A69A618" style=3D"margin:0;outline:n=
+	one;padding:0;color:#045fb4;text-decoration:underline;font-size:19px" targe=
+	t=3D"_blank"><span><span style=3D"color:inherit;font-size:inherit;font-weig=
+	ht:bold;line-height:inherit;text-decoration:inherit">Silk=E2=80=99n Stop - =
+	</span> <span style=3D"color:inherit;font-size:inherit;font-weight:bold;lin=
+	e-height:inherit;text-decoration:inherit"><span style=3D"color:#045fb4;font=
+	-size:inherit;font-weight:inherit;line-height:inherit;text-decoration:inher=
+	it;font-family:arial;font-style:normal">Hack Your Period to Stop the Pain!<=
+	/span>
+	</span></span></a><br><br><div style=3D"margin:0;outline:none;padding:0;fon=
+	t-weight:bold;text-align:left"><span style=3D"color:#084b8a;font-size:inher=
+	it;font-weight:bold;line-height:inherit;text-decoration:inherit;font-family=
+	:arial;text-align:inherit"> <a href=3D"http://silkncure.acemlnc.com/lt.php?=
+	s=3Dcbfc8d9a9a09b07c2c5410626a8a504d&amp;i=3D137A278A69A618" style=3D"margi=
+	n:0;outline:none;padding:0;color:#045fb4;text-decoration:underline;font-fam=
+	ily:arial;font-weight:400;font-style:normal;font-size:16px" target=3D"_blan=
+	k"><span>Click here</span></a><span style=3D"color:#084b8a;font-size:16px;f=
+	ont-weight:400;line-height:inherit;text-decoration:inherit;font-family:aria=
+	l;font-style:normal"> to get your LAUNCH SALE for Silk=E2=80=99n Stop.</spa=
+	n> </span></div>
+	</span></div>
+	<br style=3D"color:#084b8a"><span style=3D"color:#084b8a;font-size:16px;fon=
+	t-weight:inherit;line-height:inherit;text-decoration:inherit;font-family:ar=
+	ial;font-style:normal">This </span><span style=3D"color:#084b8a;font-size:1=
+	6px;font-weight:inherit;line-height:inherit;text-decoration:inherit;font-fa=
+	mily:arial;font-style:normal">LAUNCH SALE </span><span style=3D"color:#084b=
+	8a;font-size:16px;font-weight:inherit;line-height:inherit;text-decoration:i=
+	nherit;font-family:arial;font-style:normal">offer is for 72 hours only. <br=
+	>Delivery by Christmas guaranteed. <br>DON&#39;T MISS OUT!! </span><span st=
+	yle=3D"color:#084b8a;font-size:16px;font-weight:inherit;line-height:inherit=
+	;text-decoration:inherit;font-family:arial;font-style:normal">(Limited to t=
+	he first 29 units)<br><span style=3D"color:inherit;font-size:12px;font-weig=
+	ht:inherit;line-height:inherit;text-decoration:inherit;font-family:arial;fo=
+	nt-style:normal"><span style=3D"color:#084b8a;font-size:16px;font-weight:in=
+	herit;line-height:inherit;text-decoration:inherit;font-family:arial;font-st=
+	yle:normal"><br>Silk=E2=80=99n Stop </span><span style=3D"color:#084b8a;fon=
+	t-size:16px;font-weight:inherit;line-height:inherit;text-decoration:inherit=
+	;font-family:arial;font-style:normal">works With Your Body to Naturally Rel=
+	ieve Menstrual Pain =E2=80=93 </span><span style=3D"color:#084b8a;font-size=
+	:16px;font-weight:inherit;line-height:inherit;text-decoration:inherit;font-=
+	family:arial;font-style:italic">No pills, no chemicals, no side effects</sp=
+	an><br></span><span style=3D"color:#084b8a;font-size:12px;font-weight:inher=
+	it;line-height:inherit;text-decoration:inherit;font-family:arial;font-style=
+	:normal"><br><span style=3D"color:#084b8a;font-size:16px;font-weight:inheri=
+	t;line-height:inherit;text-decoration:inherit;font-family:arial;font-style:=
+	normal">Enjoy your Silk&#39;n Stop. Pain-free period starts today :)</span>=
+	<br><br><span style=3D"color:#084b8a;font-size:16px;font-weight:inherit;lin=
+	e-height:inherit;text-decoration:inherit;font-family:arial;font-style:norma=
+	l">Jane from Silk&#39;n</span></span></span><br style=3D"color:#084b8a"></s=
+	pan></div>
+	<div style=3D"margin:0;outline:none;padding:0"><div style=3D"margin:0;outli=
+	ne:none;padding:0"><div style=3D"margin:0;outline:none;padding:0;font-size:=
+	10px"><p style=3D"margin:0;outline:none;padding:0;color:inherit;font-size:i=
+	nherit;font-weight:inherit;line-height:inherit;text-decoration:inherit"></p=
+	>
+	 </div>
+	 </div>
+	 </div>
+	 </div>
+	 </div>
+	 </div>
+	 </div>
+	</td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	<tr style=3D"background-color:#ffffff"><td id=3D"m_-9101626047882966279layo=
+	ut-row-margin816" valign=3D"top" style=3D"background-color:#ffffff"><table =
+	width=3D"100%" border=3D"0" cellpadding=3D"0" cellspacing=3D"0" style=3D"fo=
+	nt-size:13px;min-width:100%"><tbody><tr id=3D"m_-9101626047882966279layout-=
+	row816" class=3D"m_-9101626047882966279layout m_-9101626047882966279layout-=
+	row m_-9101626047882966279widget m_-9101626047882966279_widget_picture" ali=
+	gn=3D"left"><td id=3D"m_-9101626047882966279layout-row-padding816" valign=
+	=3D"top"><table width=3D"100%" border=3D"0" cellpadding=3D"0" cellspacing=
+	=3D"0" style=3D"font-size:13px;min-width:100%"><tbody><tr><td class=3D"m_-9=
+	101626047882966279image-td" align=3D"left" valign=3D"top" width=3D"650"><im=
+	g src=3D"http://silkncure.imgus11.com/public//daf5096c75c7f97c61a437e5bce56=
+	196.png?r=3D561771177" alt=3D"" width=3D"104" style=3D"display:block;border=
+	:none;outline:none;width:104px;opacity:1;max-width:100%"></td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	<tr style=3D"background-color:#ffffff"><td id=3D"m_-9101626047882966279layo=
+	ut-row-margin815" valign=3D"top" style=3D"padding:0 50px 0 50px;background-=
+	color:#ffffff"><table width=3D"100%" border=3D"0" cellpadding=3D"0" cellspa=
+	cing=3D"0" style=3D"font-size:13px;min-width:100%;border-collapse:initial!i=
+	mportant"><tbody><tr id=3D"m_-9101626047882966279layout-row815" class=3D"m_=
+	-9101626047882966279layout m_-9101626047882966279layout-row m_-910162604788=
+	2966279widget m_-9101626047882966279_widget_break m_-9101626047882966279sty=
+	le815"><td id=3D"m_-9101626047882966279layout-row-padding815" valign=3D"top=
+	" style=3D"line-height:0;padding:0"><table width=3D"100%" border=3D"0" cell=
+	padding=3D"0" cellspacing=3D"0" style=3D"font-size:13px;min-width:100%;bord=
+	er-collapse:collapse;line-height:0"><tbody><tr><td height=3D"5" style=3D"li=
+	ne-height:0"></td>
+	</tr>
+	<tr><td align=3D"center" height=3D"1" width=3D"550" style=3D"line-height:0"=
+	> <table align=3D"center" border=3D"0" cellpadding=3D"0" cellspacing=3D"0" =
+	height=3D"1" width=3D"550" style=3D"font-size:13px;min-width:auto!important=
+	;border-collapse:collapse;line-height:0;width:100%;max-width:100%"><tbody><=
+	tr><td class=3D"m_-9101626047882966279break-line" bgcolor=3D"#d2d4db" heigh=
+	t=3D"1" width=3D"550" style=3D"line-height:1px;height:1px;width:550px;backg=
+	round-color:#d2d4db"> </td>
+	 </tr>
+	</tbody></table>
+	</td>
+	 </tr>
+	<tr><td height=3D"5" style=3D"line-height:0"></td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	</tbody></table>
+	</td>
+	</tr>
+	<tr style=3D"background-color:#ffffff"><td id=3D"m_-9101626047882966279layo=
+	ut-row-margin814" valign=3D"top" style=3D"padding:0;background-color:#fffff=
+	f"><table width=3D"100%" border=3D"0" cellpadding=3D"0" cellspacing=3D"0" s=
+	tyle=3D"font-size:13px;min-width:100%;border-collapse:initial!important"><t=
+	body><tr id=3D"m_-9101626047882966279layout-row814" class=3D"m_-91016260478=
+	82966279layout m_-9101626047882966279layout-row m_-9101626047882966279widge=
+	t m_-9101626047882966279_widget_text m_-9101626047882966279style814" style=
+	=3D"margin:0;padding:0;background-color:#ffffff"><td id=3D"m_-9101626047882=
+	966279layout-row-padding814" valign=3D"top" style=3D"background-color:#ffff=
+	ff;padding:30px 0 30px 0"><table width=3D"100%" border=3D"0" cellpadding=3D=
+	"0" cellspacing=3D"0" style=3D"font-size:13px;min-width:100%"><tbody><tr><t=
+	d id=3D"m_-9101626047882966279text_div787" class=3D"m_-9101626047882966279t=
+	d_text m_-9101626047882966279td_block" valign=3D"top" align=3D"left" style=
+	=3D"color:inherit;font-size:12px;font-weight:inherit;line-height:1;text-dec=
+	oration:inherit;font-family:Arial"> <div style=3D"margin:0;outline:none;pad=
+	ding:0;font-size:12px;color:#8904b1"> <span style=3D"color:#8904b1;font-siz=
+	e:12px;font-weight:inherit;line-height:inherit;text-decoration:inherit"> </=
+	span><div style=3D"margin:0;outline:none;padding:0;text-align:center"><div =
+	style=3D"margin:0;outline:none;padding:0;text-align:center"></div>
+	</div>
+	 <div style=3D"margin:0;outline:none;padding:0;font-family:arial;color:#084=
+	b8a;font-size:12px;font-style:normal;font-weight:400;text-align:center"><di=
+	v style=3D"margin:0;outline:none;padding:0;font-family:arial;text-align:cen=
+	ter;color:#084b8a"><a href=3D"http://silkncure.acemlnc.com/proc.php?nl=3D69=
+	&amp;c=3D137&amp;m=3D278&amp;s=3Dcbfc8d9a9a09b07c2c5410626a8a504d&amp;act=
+	=3Dunsub" style=3D"margin:0;outline:none;padding:0;color:#045fb4;text-decor=
+	ation:underline;font-family:arial" target=3D"_blank"><span style=3D"color:i=
+	nherit;font-size:inherit;font-weight:inherit;line-height:inherit;text-decor=
+	ation:inherit;font-family:arial">Click here to unsubscribe</span></a><br><b=
+	r></div>
+	<br style=3D"font-family:arial;font-weight:inherit;color:#084b8a;font-size:=
+	12px;font-family:arial;font-size:12px;color:#084b8a;color:#084b8a"></div>
+	<div style=3D"margin:0;outline:none;padding:0;font-family:arial;color:#084b=
+	8a;font-size:12px;font-style:normal;font-weight:400;text-align:center"><spa=
+	n style=3D"color:#084b8a;font-size:12px;font-weight:inherit;line-height:inh=
+	erit;text-decoration:inherit;font-family:arial"><b style=3D"margin:0;outlin=
+	e:none;padding:0;font-family:arial;color:#084b8a;font-style:normal;font-wei=
+	ght:normal"><span style=3D"color:#084b8a;font-size:inherit;font-weight:400;=
+	line-height:inherit;text-decoration:inherit;font-family:arial;font-style:no=
+	rmal">Silk&#39;n Therapy, POB 533 , 20692 Yokneam , Israel</span></b> </spa=
+	n></div>
+  `;
+};
+
+
