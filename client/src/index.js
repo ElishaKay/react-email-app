0afd8396aac8af87aceae30bb6407a66fb2d1f23
elishakay <kramer1346@gmail.com>
2018-10-19 00:51:10 +0300

    added redux devtools to dev environment

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -12,7 +12,25 @@ import reducers from './reducers';
 import axios from 'axios';
 window.axios = axios;
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const middleware = [thunk];
+let store = {}
+
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(reducers,
+		{},
+		compose(
+	    applyMiddleware(...middleware)
+	  )
+	);
+} else {
+  store = createStore(reducers,
+		{},
+		compose(
+	    applyMiddleware(...middleware),
+	    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	  )
+	);
+}
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
