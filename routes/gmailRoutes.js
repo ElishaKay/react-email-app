f03465a7d78baee7439e9832643f223d4f4d5a33
elishakay <kramer1346@gmail.com>
2018-12-22 19:24:33 +0200

    logs the users gmail labels in the nodejs console when running node gmailRoutes.js

--- routes/gmailRoutes.js
+++ routes/gmailRoutes.js
@@ -1,10 +1,137 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
+var fs = require('fs');
+var readline = require('readline');
+var {google} = require('googleapis');
+ 
+// If modifying these scopes, delete your previously saved credentials
+// at TOKEN_DIR/gmail-nodejs.json
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+ 
+// Change token directory to your system preference
+var TOKEN_DIR = (__dirname+'/');
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs.json';
+ 
+var gmail = google.gmail('v1');
+ 
+// Authorize a client with the loaded credentials, then call the
+// Gmail API.
+
+
+authorize(listLabels);
+ 
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(callback) {
+    var clientSecret = process.env.CLIENT_SECRET;
+    var clientId = process.env.CLIENT_ID;
+    var redirectUrl = process.env.REDIRECT_DOMAIN;
+ 
+    var OAuth2 = google.auth.OAuth2;
+ 
+    var oauth2Client = new OAuth2(clientId, clientSecret,  redirectUrl);
+    console.log('oauth2Client: ',oauth2Client)
+ 
+    let token = {access_token: "ya29.GmB6BsbAouAI0wpYmmL77RzC2sfcZ6d8xsFFqyPxKxVbbiDXbNNrbzH2jQJDHL8jzW91Yx2mjJgU1xVy7GemJSa7VmAMXYycN-A6kkdLm8sGlOVDRcOKcucWYr_Odp4M1fk",
+                  scope: "https://www.googleapis.com/auth/gmail.readonly",
+                  token_type: "Bearer"};
+
+      oauth2Client.credentials = token;
+      callback(oauth2Client);
+}
+ 
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({access_type: 'offline', scope: SCOPES});
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+ 
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+
+      console.log('oauth2Client: ',oauth2Client);
+      console.log('callback: ',callback)
+
+      callback(oauth2Client);
+    });
+  });
+}
+ 
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  console.log('heres the token: ',token)
+
+  // try {
+  //   fs.mkdirSync(TOKEN_DIR);
+  // } catch (err) {
+  //   if (err.code != 'EEXIST') {
+  //     throw err;
+  //   }
+  // }
+  // fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  // console.log('Token stored to ' + TOKEN_PATH);
+}
+ 
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  gmail.users.labels.list({auth: auth, userId: 'me',}, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+ 
+    var labels = response.data.labels;
+ 
+    if (labels.length == 0) {
+      console.log('No labels found.');
+    } else {
+      console.log('Labels:');
+      for (var i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        console.log('%s', label.name);
+      }
+    }
+  });
+}
+
 
 module.exports = app => {
   app.post('/api/sendEmail', requireLogin, async (req, res) => {
-   
+    
+    let {accessToken} = req.user;
+
+
     /**
      * Lists the labels in the user's account.
      *
