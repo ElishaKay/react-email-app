cc606c29a8eba82ce2b391cd50bad0e13a1f93df
elishakay <kramer1346@gmail.com>
2018-12-28 11:40:02 +0200

    dropdown works but shitty ux

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Payments from './Payments';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class Header extends Component {
   renderContent() {
---
+++ client/src/components/Header2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+export class Header2 extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.state = { isOpen: false }
+  }
+
+  handleOpen() {
+    this.setState({ isOpen: true })
+  }
+
+  handleClose() {
+     this.setState({ isOpen: false })
+  }
+
+  render() {
+    return (
+       <Nav>
+        <NavDropdown
+          onMouseEnter = { this.handleOpen }
+          onMouseLeave = { this.handleClose }
+          open={ this.state.isOpen }
+          noCaret
+          id="language-switcher-container"
+        >
+          <MenuItem>Only one Item</MenuItem>
+        </NavDropdown>
+      </Nav>
+    )
+  }
+}
+
+export default Header2;
